a26f1bb54a4fe7582aa4c4f0221be53e45b8518c
deal-machine <95deal@gmail.com>
2022-10-03 19:43:58 -0300

    create error protocols

---
+++ mocks/error/protocols.js
@@ -0,0 +1,9 @@
+const errors = {
+  error: {
+    FILE_LENGTH: "The content length is invalid!",
+    FILE_FIELDS: "The fields provided are incorrect!",
+    FILE_HEADERS: "The header are incompatible!",
+  },
+};
+
+module.exports = errors;
