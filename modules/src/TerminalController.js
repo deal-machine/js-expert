6a32c9c56fdfde846c31a96801d4cf9689a175af
deal-machine <95deal@gmail.com>
2023-07-03 16:24:58 -0300

    refactor: create terminal controller

---
+++ modules/src/TerminalController.js
@@ -0,0 +1,45 @@
+import readline from "readline";
+import DraftLog from "draftlog";
+import chalk from "chalk";
+import chalkTable from "chalk-table";
+import { Person } from "./models/Person.js";
+
+export class TerminalController {
+  constructor() {
+    this.print = {};
+    this.data = {};
+  }
+
+  initializeTerminal(database, language) {
+    DraftLog(console).addLineListener(process.stdin);
+    this.terminal = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    this.initializeTable(database, language);
+  }
+
+  initializeTable(database, language) {
+    const data = database.map((item) => new Person(item).formatted(language));
+    const table = chalkTable(this.getTableOptions(), data);
+    this.print = console.draft(table);
+    this.data = data;
+  }
+
+  question(msg = "") {
+    return new Promise((resolve) => this.terminal.question(msg, resolve));
+  }
+
+  getTableOptions() {
+    return {
+      leftPad: 5,
+      columns: [
+        { field: "id", name: chalk.cyan("ID") },
+        { field: "vehicles", name: chalk.magenta("Vehicles") },
+        { field: "distance", name: chalk.red("Distance") },
+        { field: "from", name: chalk.blue("From") },
+        { field: "to", name: chalk.blueBright("To") },
+      ],
+    };
+  }
+}
--- modules/src/index.js
+++ modules/src/index.js
@@ -1,27 +1,7 @@
-import DraftLog from "draftlog";
-import chalk from "chalk";
-import chalkTable from "chalk-table";
-
 import database from "../database.json" assert { type: "json" };
-import { Person } from "./models/Person.js";
+import { TerminalController } from "./TerminalController.js";
 
 const DEFAULT_LANG = "pt-BR";
 
-DraftLog(console).addLineListener(process.stdin);
-
-const options = {
-  leftPad: 5,
-  columns: [
-    { field: "id", name: chalk.cyan("ID") },
-    { field: "vehicles", name: chalk.magenta("Vehicles") },
-    { field: "distance", name: chalk.red("Distance") },
-    { field: "from", name: chalk.blue("From") },
-    { field: "to", name: chalk.blueBright("To") },
-  ],
-};
-
-const table = chalkTable(
-  options,
-  database.map((item) => new Person(item).formatted(DEFAULT_LANG))
-);
-console.draft(table);
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, DEFAULT_LANG);
